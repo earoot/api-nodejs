ee4aa6d02f59d4168ecd2f984ee2a3bfa0681800
Elías Schotborgh <eliasschotborgh@MacBook-Pro.local>
2020-11-15 10:34:19 -0300

    Testing ls

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 echo "Archivo Deploy SH"
 ls
+echo "abajo"
+ls $CODEBUILD_SRC_DIR/
 npm install -g serverless
-serverless deploy --stage dev
+serverless deploy --stage dev --package $CODEBUILD_SRC_DIR/target/dev -v -r us-east-2
