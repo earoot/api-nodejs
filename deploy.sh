06b9f3181d6ad379f4913cea21c295b0be5da64c
Elías Schotborgh <eliasschotborgh@MacBook-Pro.local>
2020-11-15 00:42:58 -0300

    Testing

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 echo "Archivo Deploy SH"
 npm install -g serverless
-serverless deploy --stage $env --package \   $CODEBUILD_SRC_DIR/target/dev -v -r us-east-2
+serverless deploy --stage dev --package \   $CODEBUILD_SRC_DIR/target/dev -v -r us-east-2
