d2c452aa45c89bf58f3543662d095be83bc2d90b
Elías Schotborgh <eliasschotborgh@MacBook-Pro.local>
2020-11-15 10:12:09 -0300

    Removed sudo

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 echo "Archivo Deploy SH"
 npm install -g serverless
-sudo -s serverless deploy --stage dev --package \   $CODEBUILD_SRC_DIR/target/dev -v -r us-east-2
+serverless deploy --stage dev --package \   $CODEBUILD_SRC_DIR/target/dev -v -r us-east-2
