12c97a7ab66a94a21abf256af5e66cadbd801e81
Elías Schotborgh <eliasschotborgh@MacBook-Pro.local>
2020-11-14 22:19:20 -0300

    Initial Commit, added signup and login endpoint

---
+++ routes/routes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+//Controllers
+const userController = require('../Controllers/userController');
+
+//Models
+const User = require('../Models/userModel');
+
+// const jwt = require('jsonwebtoken');
+// const helper = require('../helper.js');
+
+// const validateToken = async (req, res, next) => {
+//   if (req.headers["Authorization"]) {
+//     try {
+//       const accessToken = req.headers["Authorization"];
+//       const { userId, exp } = await jwt.verify(accessToken, process.env.JWT_SECRET);
+//       if (exp < Date.now().valueOf() / 1000) {
+//         return helper.responseJson(res, 401, 'The session has expired', {});
+//       }
+//       res.locals.user = await User.findById(userId);
+//       next();
+//     } catch (error) {
+//       return helper.responseJson(res, 401, 'The session has expired', {});
+//     }
+//   } else {
+//     return helper.responseJson(res, 401, 'This credentials does not match our records', {});
+//   }
+// };
+
+router.post('/login', userController.login);
+router.post('/signup', userController.register);
+
+module.exports = router;
